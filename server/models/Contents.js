eb946ad7a250baa4ef2b0e7aab75e9a2cb4ed48c
2uwls1126 <[2uwls1126@g.skku.edu]>
2023-08-07 20:21:57 +0900

    model schema

--- server/models/Contents.js
+++ server/models/Contents.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const productSchema = mongoose.Schema({
+const contentSchema = mongoose.Schema({
+    category: {
+        type: String,
+    }
+    ,subcategory: {
+        type: String
+
+    }
+    ,name: {
+        type: String
+    }
+    ,level: {
+        type: Number
+
+    }
     
-
-
-
-
 })
+
+const Content=mongoose.model('Content',contentSchema)
+module.exports={Content}
