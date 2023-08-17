16cd9af78fd05e11c1c82161e28c5ddbb4dda750
2uwls1126 <[2uwls1126@g.skku.edu]>
2023-08-17 10:39:27 +0900

    modify img error

--- frontend/src/shop.js
+++ frontend/src/shop.js
@@ -309,7 +309,7 @@ function Card() {
                 {data.view}
               </div>
               <a
-                href={`/detail?category=${data.category}&name=${data.name}&subcategory=${data.subcategory}&level=${data.level}%image=${data.image}`}
+                href={`/detail?&name=${data.name}&subcategory=${data.subcategory}&level=${data.level}&image=${data.image}`}
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 more
