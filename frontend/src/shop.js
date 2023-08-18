7e7d2811cabe3985663d937c6fb04b408a96f0aa
shhyunn <limgm7@gmail.com>
2023-08-18 20:59:03 +0900

    apply pagination for shop page

--- frontend/src/shop.js
+++ frontend/src/shop.js
@@ -10,7 +10,6 @@ import imageVariables from "./imagevariables";
 const dataModule = require("./data");
 const kioskItems = dataModule.kioskItems;
 
-
 function Total() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("패스트푸드");
@@ -26,26 +25,24 @@ function Total() {
     setSearchTerm(itemText);
     setDropdownOpen(false);
   };
-  const [inputValue, setInputValue] = useState("");
+  //const [inputValue, setInputValue] = useState("");
 
   // 버튼 클릭 핸들러
   const handleButtonClick = (name) => {
     setSelectedCategory(selectedCategory);
     setSearchTerm(name);
   };
-  
-
 
   //Input
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+    setSearchTerm(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // 검색어 사용 또는 상태 업데이트 등의 작업 수행
   };
-  
+
   useEffect(() => {
     if (selectedCategory === "전체") {
       const filteredItems = searchData(searchTerm, selectedCategory);
@@ -55,7 +52,6 @@ function Total() {
       setFilteredKioskItems(filteredItems);
     }
   }, [selectedCategory, searchTerm]);
-
 
   const StarIcon = ({ colorClass }) => (
     <svg
@@ -105,7 +101,24 @@ function Total() {
       return false;
     });
   };
-  
+
+  //pagination
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12; // 예: 페이지당 8개의 아이템을 표시
+
+  const totalPages = Math.ceil(filteredKioskItems.length / itemsPerPage);
+
+  const handleChangePage = (newPage) => {
+    if (newPage > 0 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const displayedData = filteredKioskItems.slice(startIndex, endIndex);
 
   return (
     <div class="w-full h-full">
@@ -129,92 +142,100 @@ function Total() {
             }}
           >
             <div class="items-center justify-center">
-      <div>
-        <div className="flex mt-8">
-          {/* 드롭다운 메뉴 추가 */}
-          <div className="flex mr-8">
-            <button
-              id="dropdown-button"
-              className={`flex-shrink-0 z-10 inline-flex items-center py-2.5 px-5 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 ${
-                isDropdownOpen ? "rounded-lg" : "rounded-lg"
-              }`}
-              style={{ width: "130px", justifyContent: "space-between" }}
-              onClick={toggleDropdown}
-              type="button"
-            >
-              <span className="flex items-center">
-                {selectedCategory === "true" ? "패스트푸드" : selectedCategory}
-              </span>
-              <svg
-                className={`w-2.5 h-2.5 transform ${
-                  isDropdownOpen ? "rotate-180" : "rotate-0"
-                }`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 2 4 4 4-4"
-                />
-              </svg>
-            </button>
-            {isDropdownOpen && (
-              <div
-                id="dropdown"
-                className="absolute mt-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-              >
-                <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdown-button"
-                >
-                  <li>
+              <div>
+                <div className="flex mt-8">
+                  {/* 드롭다운 메뉴 추가 */}
+                  <div className="flex mr-8">
                     <button
+                      id="dropdown-button"
+                      className={`flex-shrink-0 z-10 inline-flex items-center py-2.5 px-5 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 ${
+                        isDropdownOpen ? "rounded-lg" : "rounded-lg"
+                      }`}
+                      style={{
+                        width: "130px",
+                        justifyContent: "space-between",
+                      }}
+                      onClick={toggleDropdown}
                       type="button"
-                      className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      onClick={() => handleDropdownItemClick("패스트푸드")}
                     >
-                      패스트푸드
+                      <span className="flex items-center">
+                        {selectedCategory === "true"
+                          ? "패스트푸드"
+                          : selectedCategory}
+                      </span>
+                      <svg
+                        className={`w-2.5 h-2.5 transform ${
+                          isDropdownOpen ? "rotate-180" : "rotate-0"
+                        }`}
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 10 6"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m1 2 4 4 4-4"
+                        />
+                      </svg>
                     </button>
-                  </li>
-                  <li>
-                    <button
-                      type="button"
-                      className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      onClick={() => handleDropdownItemClick("카페")}
-                    >
-                      카페
-                    </button>
-                  </li>
-                  <li>
-                    <button
-                      type="button"
-                      className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      onClick={() => handleDropdownItemClick("영화관")}
-                    >
-                      영화관
-                    </button>
-                  </li>
-                  <li>
-                    <button
-                      type="button"
-                      className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      onClick={() => handleDropdownItemClick("주차시스템")}
-                    >
-                      주차시스템
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-          <div className="relative w-full">
-            
-            <form onSubmit={handleSubmit}>
+                    {isDropdownOpen && (
+                      <div
+                        id="dropdown"
+                        className="absolute mt-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                      >
+                        <ul
+                          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                          aria-labelledby="dropdown-button"
+                        >
+                          <li>
+                            <button
+                              type="button"
+                              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                              onClick={() =>
+                                handleDropdownItemClick("패스트푸드")
+                              }
+                            >
+                              패스트푸드
+                            </button>
+                          </li>
+                          <li>
+                            <button
+                              type="button"
+                              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                              onClick={() => handleDropdownItemClick("카페")}
+                            >
+                              카페
+                            </button>
+                          </li>
+                          <li>
+                            <button
+                              type="button"
+                              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                              onClick={() => handleDropdownItemClick("영화관")}
+                            >
+                              영화관
+                            </button>
+                          </li>
+                          <li>
+                            <button
+                              type="button"
+                              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                              onClick={() =>
+                                handleDropdownItemClick("주차시스템")
+                              }
+                            >
+                              주차시스템
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                  <div className="relative w-full">
+                    <form onSubmit={handleSubmit}>
                       <input
                         type="text"
                         id="search-dropdown"
@@ -224,14 +245,14 @@ function Total() {
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
                       />
-              <button
-                type="submit"
-                className="absolute top-0 right-0 px-5 py-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                검색
-              </button>
-            </form>
-          </div>
+                      <button
+                        type="submit"
+                        className="absolute top-0 right-0 px-5 py-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        검색
+                      </button>
+                    </form>
+                  </div>
                 </div>
               </div>
               <div>
@@ -239,7 +260,7 @@ function Total() {
                   <button
                     type="button"
                     data-value="맥도날드"
-                    onClick={() => handleButtonClick("맥도날드")} 
+                    onClick={() => handleButtonClick("맥도날드")}
                     class="text-[16px] text-gray-500 hover:text-white bg-white hover:bg-indigo-300 focus:ring-1 focus:outline-none focus:ring-indigo-300 font-bold rounded-full px-2 py-2 text-center mb-2 w-32"
                   >
                     맥도날드
@@ -248,7 +269,6 @@ function Total() {
                     type="button"
                     data-value="베스킨라빈스"
                     onClick={() => {
-
                       handleButtonClick("배스킨라빈스");
                     }}
                     class="text-[16px] text-gray-500 hover:text-white bg-white hover:bg-indigo-300 focus:ring-1 focus:outline-none focus:ring-indigo-300 font-bold rounded-full px-2 py-2 text-center mb-2 w-32"
@@ -259,17 +279,16 @@ function Total() {
                     type="button"
                     data-value="KFC"
                     onClick={() => {
-
                       handleButtonClick("KFC");
                     }}
                     class="text-[16px] text-gray-500 hover:text-white bg-white hover:bg-indigo-300 focus:ring-1 focus:outline-none focus:ring-indigo-300 font-bold rounded-full px-2 py-2 text-center mb-2 w-32"
                   >
-                   KFC
+                    KFC
                   </button>
                   <button
                     type="button"
                     data-value="버거킹"
-                    onClick={() => handleButtonClick("버거킹")} 
+                    onClick={() => handleButtonClick("버거킹")}
                     class="text-[16px] text-gray-500 hover:text-white bg-white hover:bg-indigo-300 focus:ring-1 focus:outline-none focus:ring-indigo-300 font-bold rounded-full px-2 py-3 text-center mb-2 w-32"
                   >
                     버거킹
@@ -277,7 +296,7 @@ function Total() {
                   <button
                     type="button"
                     data-value="샐러디"
-                    onClick={() => handleButtonClick("샐러디")}  // 버튼을 누를 때 검색어를 "샐러디"로 설정
+                    onClick={() => handleButtonClick("샐러디")} // 버튼을 누를 때 검색어를 "샐러디"로 설정
                     class="text-[16px] text-gray-500 hover:text-white bg-white hover:bg-indigo-300 focus:ring-1 focus:outline-none focus:ring-indigo-300 font-bold rounded-full px-2 py-2 text-center mb-2 w-32"
                   >
                     샐러디
@@ -290,7 +309,7 @@ function Total() {
       </div>
       <div>
         <div className="grid gird-cols-2 md:grid-cols-4 p-20 gap-10">
-          {filteredKioskItems.map((data, index) => (
+          {displayedData.map((data, index) => (
             <div key={index}>
               <div className="sw-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex justify-center p-5">
@@ -339,98 +358,24 @@ function Total() {
         </div>
       </div>
       <div class="flex items-center justify-center">
-        <div>
-          <nav aria-label="Page navigation example">
-            <ul class="flex items-center -space-x-px h-8 text-sm">
-              <li>
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  <span class="sr-only">Previous</span>
-                  <svg
-                    class="w-2.5 h-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M5 1 1 5l4 4"
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  aria-current="page"
-                  class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                >
-                  1
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  2
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  3
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  4
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  5
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  <span class="sr-only">Next</span>
-                  <svg
-                    class="w-2.5 h-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m1 9 4-4-4-4"
-                    />
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </nav>
+        <div className="flex justify-center mt-5">
+          <button
+            className="mx-2 p-2 border rounded hover:bg-blue-100 hover:font-extrabold hover:text-blue-600"
+            onClick={() => handleChangePage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            이전
+          </button>
+          <span class="items-center justify-center mx-5 text-gray-700 text-md">
+            {currentPage}/{totalPages}
+          </span>
+          <button
+            className="mx-2 p-2 border rounded hover:bg-blue-100 hover:font-extrabold hover:text-blue-600"
+            onClick={() => handleChangePage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            다음
+          </button>
         </div>
       </div>
     </div>
